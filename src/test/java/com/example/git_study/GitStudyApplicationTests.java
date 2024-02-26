399a30d308ee3ea8995f8bc0410aebb84bf3d00e
yeah0203 <yeah0203@naver.com>
2024-02-27 07:13:27 +0900

    init commit

---
+++ src/test/java/com/example/git_study/GitStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.git_study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
